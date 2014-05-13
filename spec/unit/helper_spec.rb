59facf6107482df5d0ff1d121a418dbc61267509
Julian C. Dunn <jdunn@getchef.com>
2014-05-13 16:55:40 -0500

    Fix broken test under Chef 11.12.4. Not the prettiest but it works now

--- spec/unit/helper_spec.rb
+++ spec/unit/helper_spec.rb
@@ -23,10 +23,13 @@ describe Demo::Helper do
     let(:shellout) { double(run_command: nil, error!: nil, stdout: '', stderr: double(empty?: true)) }
     let(:dummy_class) { Class.new { include Demo::Helper } }
 
-    before { Mixlib::ShellOut.stub(:new).and_return(shellout) }
+    before {
+      Mixlib::ShellOut.stub(:new).and_return(shellout)
+    }
 
     it 'builds the correct command' do
       expect(Mixlib::ShellOut).to receive(:new).with('getent passwd bacon', {:returns=>[0, 2]})
+      expect(shellout).to receive(:live_stream=).and_return(nil)
       expect(dummy_class.new.has_bacon?).to be_false
     end
 
@@ -35,6 +38,7 @@ describe Demo::Helper do
       before { Mixlib::ShellOut.stub(:new).and_return(shellout) }
 
       it 'says there is no bacon' do
+        expect(shellout).to receive(:live_stream=).and_return(nil)
         expect(dummy_class.new.has_bacon?).to be_false
       end
     end
@@ -44,6 +48,7 @@ describe Demo::Helper do
       before { Mixlib::ShellOut.stub(:new).and_return(shellout) }
 
       it 'says there is bacon' do
+        expect(shellout).to receive(:live_stream=).and_return(nil)
         expect(dummy_class.new.has_bacon?).to be_true
       end
     end
